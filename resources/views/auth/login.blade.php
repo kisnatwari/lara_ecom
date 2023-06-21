c2c719f62d8573e0b40458fabf59efe97dfd1d5f
Krishna Tiwari <kisnatwari@gmail.com>
2023-06-21 07:46:45 +0545

    authentication page UI improved

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,27 @@
-<x-guest-layout>
+@extends('layouts.auth')
+
+@section('auth-form')
+<x-auth-session-status class="mb-4" :status="session('status')" />
+    <form action="{{ route('login') }}" method="POST" class="flex flex-col gap-3 mx-3">
+        @csrf
+        <div class="flex flex-col items-center justify-center">
+            <h1 class="text-white text-4xl font-bold">Upabhog</h1>
+            <p class="text-white text-lg">Login here to get started.</p>
+        </div>
+        <div class=" bg-slate-700 bg-opacity-50 p-5 md:w-[300px] flex flex-col gap-3">
+            <x-input-group id="email" placeholder="example@domain.com" label="Email ID" class="dark:bg-slate-900" />
+            <x-input-group id="password" type="password" placeholder="Password" label="Password" class="dark:bg-slate-900" />
+            <div class="text-right">
+                <button class="bg-indigo-700 py-2 w-full rounded-md px-7 text-white">Login</button>
+            </div>
+        </div>
+        <div class="text-slate-300 text-right text-sm">
+            <a href="/register">Don't have an account?</a>
+        </div>
+    </form>
+@endsection
+
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -45,3 +68,4 @@
         </div>
     </form>
 </x-guest-layout>
+ --}}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,61 @@
-<x-guest-layout>
+@extends('layouts.auth')
+
+@section('auth-form')
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <form action="{{ route('register') }}" method="POST" class="flex flex-col gap-3 mx-3">
+        @csrf
+        <div class="flex flex-col items-center justify-center">
+            <h1 class="text-white text-4xl font-bold">Upabhog</h1>
+            <p class="text-white text-lg">Create an account to get started.</p>
+        </div>
+        <div class=" bg-slate-700 bg-opacity-50 p-5 md:w-[400px] flex flex-col gap-3">
+            <div>
+                <x-input-label for="name" :value="__('Name')" />
+                <x-text-input id="name" class="block mt-1 w-full dark:bg-slate-900/50" type="text" name="name"
+                    :value="old('name')" required autofocus autocomplete="name" placeholder="John Doe" />
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            </div>
+
+            <!-- Email Address -->
+            <div>
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input id="email" class="block mt-1 w-full dark:bg-slate-900/50" type="email" name="email"
+                    :value="old('email')" required autocomplete="username" placeholder="example@domain.com" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+            <!-- Password -->
+            <div>
+                <x-input-label for="password" :value="__('Password')" />
+
+                <x-text-input id="password" class="block mt-1 w-full dark:bg-slate-900/50" type="password" name="password"
+                    required autocomplete="new-password" placeholder="example@domain.com" />
+
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <!-- Confirm Password -->
+            <div>
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-text-input id="password_confirmation" class="block mt-1 w-full dark:bg-slate-900/50" type="password"
+                    name="password_confirmation" required autocomplete="new-password" placeholder="example@domain.com" />
+
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+            </div>
+            <div class="text-right">
+                <button class="bg-indigo-700 py-2 w-full rounded-md px-7 text-white">Login</button>
+            </div>
+        </div>
+        <div class="text-slate-300 text-right text-sm flex justify-between flex-wrap gap-4">
+            <a href="/login">Already have an account?</a>
+            <a href="/seller-register">Register as a seller</a>
+        </div>
+    </form>
+@endsection
+
+
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -50,3 +107,4 @@
         </div>
     </form>
 </x-guest-layout>
+ --}}
