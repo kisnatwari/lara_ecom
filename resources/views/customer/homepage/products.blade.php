46bf8f6f965625c202a975a61bf6d5d6b3f7f834
Krishna Tiwari <kisnatwari@gmail.com>
2023-06-29 19:01:42 +0545

    links to shop profile has been made for customers product card

--- resources/views/customer/homepage/products.blade.php
+++ resources/views/customer/homepage/products.blade.php
@@ -1,123 +1,132 @@
 @isset($productsFromMunicipality)
 
-<div class="w-full bg-slate-50 dark:bg-slate-800 text-slate800 dark:text-slate-200 py-2">
-    <div class="container mx-auto">
-        <h1 class="text-center text-lg flex justify-between items-end">
-            <span>Products in your area  ({{auth() -> user() -> municipality -> municipality_name}})</span>
-            <a href="#" class="text-sm dark:text-indigo-300 text-indigo-700">See All</a>
-        </h1>
-        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-2">
-            @foreach ($productsFromMunicipality as $product)
-                <a href="/products/{{ $product->id }}"
-                    class="mx-auto my-2 shadow w-44 sm:w-52 md:w-[185px] lg:w-[200px] xl:w-52 2xl:w-[185px] bg-white dark:bg-slate-900/60 dark:text-slate-200 text-slate-700 overflow-hidden rounded-md">
+    <div class="w-full bg-slate-50 dark:bg-slate-800 text-slate800 dark:text-slate-200 py-2">
+        <div class="container mx-auto">
+            <h1 class="text-center text-lg flex justify-between items-end">
+                <span>Products in your area ({{ auth()->user()->municipality->municipality_name }})</span>
+                <a href="#" class="text-sm dark:text-indigo-300 text-indigo-700">See All</a>
+            </h1>
+            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-2">
+                @foreach ($productsFromMunicipality as $product)
+                    <a href="/products/{{ $product->id }}"
+                        class="mx-auto my-2 shadow w-44 sm:w-52 md:w-[185px] lg:w-[200px] xl:w-52 2xl:w-[185px] bg-white dark:bg-slate-900/60 dark:text-slate-200 text-slate-700 overflow-hidden rounded-md">
 
-                    @php
-                        $image = json_decode($product->images, true)[0];
-                        if (strpos($image, 'https://') !== 0 && strpos($image, 'http://') !== 0) {
-                            $imageUrl = Storage::url($image);
-                        } else {
-                            $imageUrl = $image;
-                        }
-                        
-                    @endphp
-                    <img src="{{ $imageUrl }}" alt="" class="w-full h-44 sm:h-52 md:h-[185px] lg:h-[200px] xl:h-52 2xl:h-[185px] object-cover rounded-b">
-                    <div class="px-2 py-1">
-                        <h5 class="text-md line-clamp-1">{{ $product->product_name }}</h5>
-                        <p class="text-sm font-bold"><small class="text-muted">Rs {{ $product->price }}</small></p>
-                        <p class="text-xs dark:text-slate-400 text-slate-700 line-clamp-1">
-                            {{ $product->seller->shop_name }}</p>
-                    </div>
-                </a>
-            @endforeach
+                        @php
+                            $image = json_decode($product->images, true)[0];
+                            if (strpos($image, 'https://') !== 0 && strpos($image, 'http://') !== 0) {
+                                $imageUrl = Storage::url($image);
+                            } else {
+                                $imageUrl = $image;
+                            }
+                            
+                        @endphp
+                        <img src="{{ $imageUrl }}" alt=""
+                            class="w-full h-44 sm:h-52 md:h-[185px] lg:h-[200px] xl:h-52 2xl:h-[185px] object-cover rounded-b">
+                        <div class="px-2 py-1">
+                            <h5 class="text-md line-clamp-1">{{ $product->product_name }}</h5>
+                            <p class="text-sm font-bold"><small class="text-muted">Rs {{ $product->price }}</small></p>
+                            <p data-href="{{ route('shop', $product->seller->id) }}"
+                                class="dynamic-link text-sm dark:text-slate-400 text-slate-700 line-clamp-1"
+                                title="{{ $product->seller->shop_name }}">
+                                {{ $product->seller->shop_name }}
+                            </p>
+                        </div>
+                    </a>
+                @endforeach
+            </div>
         </div>
     </div>
-</div>
 @endisset
 
 @isset($productsFromDistrict)
-<div class="w-full bg-slate-50 dark:bg-slate-800 text-slate800 dark:text-slate-200 py-2">
-    <div class="container mx-auto">
-        <h1 class="text-center text-lg flex justify-between items-end">
-            <span>Products in your District  ({{auth() -> user() -> municipality -> district -> district_name}})</span>
-            <a href="#" class="text-sm dark:text-indigo-300 text-indigo-700">See All</a>
-        </h1>
-        <div
-            class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-2">
-            @foreach ($productsFromDistrict as $product)
-                <a href="/products/{{ $product->id }}"
-                    class="mx-auto my-2 shadow w-44 sm:w-52 md:w-[185px] lg:w-[200px] xl:w-52 2xl:w-[185px] dark:bg-slate-900/60 dark:text-slate-200 text-slate-700 overflow-hidden rounded-md bg-white">
+    <div class="w-full bg-slate-50 dark:bg-slate-800 text-slate800 dark:text-slate-200 py-2">
+        <div class="container mx-auto">
+            <h1 class="text-center text-lg flex justify-between items-end">
+                <span>Products in your District ({{ auth()->user()->municipality->district->district_name }})</span>
+                <a href="#" class="text-sm dark:text-indigo-300 text-indigo-700">See All</a>
+            </h1>
+            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-2">
+                @foreach ($productsFromDistrict as $product)
+                    <a href="/products/{{ $product->id }}"
+                        class="mx-auto my-2 shadow w-44 sm:w-52 md:w-[185px] lg:w-[200px] xl:w-52 2xl:w-[185px] dark:bg-slate-900/60 dark:text-slate-200 text-slate-700 overflow-hidden rounded-md bg-white">
 
-                    @php
-                        $image = json_decode($product->images, true)[0];
-                        if (strpos($image, 'https://') !== 0 && strpos($image, 'http://') !== 0) {
-                            $imageUrl = Storage::url($image);
-                        } else {
-                            $imageUrl = $image;
-                        }
-                        
-                    @endphp
-                    <img src="{{ $imageUrl }}" alt="" class="w-full h-44 sm:h-52 md:h-[185px] lg:h-[200px] xl:h-52 2xl:h-[185px] object-cover rounded-b">
-                    <div class="px-2 py-1">
-                        <h5 class="text-md line-clamp-1" title="{{ $product->product_name }}">
-                            {{ $product->product_name }}</h5>
-                        <p class="text-sm font-bold"><small class="text-muted" title="{{ $product->price }}">Rs
-                                {{ $product->price }}</small></p>
-                        <p class="text-sm dark:text-slate-400 text-slate-700 line-clamp-1"
-                            title="{{ $product->seller->shop_name }}">
-                            {{ $product->seller->shop_name }}</p>
-                        <p class="text-sm dark:text-slate-400 text-slate-700"
-                            title="{{ $product->seller->user->municipality->municipality_name }} ">
-                            {{ $product->seller->user->municipality->municipality_name }} </p>
-                    </div>
-                </a>
-            @endforeach
+                        @php
+                            $image = json_decode($product->images, true)[0];
+                            if (strpos($image, 'https://') !== 0 && strpos($image, 'http://') !== 0) {
+                                $imageUrl = Storage::url($image);
+                            } else {
+                                $imageUrl = $image;
+                            }
+                            
+                        @endphp
+                        <img src="{{ $imageUrl }}" alt=""
+                            class="w-full h-44 sm:h-52 md:h-[185px] lg:h-[200px] xl:h-52 2xl:h-[185px] object-cover rounded-b">
+                        <div class="px-2 py-1">
+                            <h5 class="text-md line-clamp-1" title="{{ $product->product_name }}">
+                                {{ $product->product_name }}</h5>
+                            <p class="text-sm font-bold"><small class="text-muted" title="{{ $product->price }}">Rs
+                                    {{ $product->price }}</small></p>
+                            <p data-href="{{ route('shop', $product->seller->id) }}"
+                                class="dynamic-link text-sm dark:text-slate-400 text-slate-700 line-clamp-1"
+                                title="{{ $product->seller->shop_name }}">
+                                {{ $product->seller->shop_name }}
+                            </p>
+                            <p class="text-sm dark:text-slate-400 text-slate-700"
+                                title="{{ $product->seller->user->municipality->municipality_name }} ">
+                                {{ $product->seller->user->municipality->municipality_name }} </p>
+                        </div>
+                    </a>
+                @endforeach
+            </div>
         </div>
     </div>
-</div>
 @endisset
 
-@if(isset($randomProducts) && count($randomProducts) > 0)
-<div class="w-full bg-slate-50 dark:bg-slate-800 text-slate800 dark:text-slate-200 py-2">
-    <div class="container mx-auto">
-        <h1 class="text-center text-lg flex justify-between items-end">
-            <span>Some Random Products</span>
-            <a href="#" class="text-sm dark:text-indigo-300 text-indigo-700">See All</a>
-        </h1>
-        <div
-            class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-2">
-            @foreach ($randomProducts as $product)
-                <a href="/products/{{ $product->id }}"
-                    class="mx-auto my-2 shadow w-44 sm:w-52 md:w-[185px] lg:w-[200px] xl:w-52 2xl:w-[185px] dark:bg-slate-900/60 dark:text-slate-200 text-slate-700 overflow-hidden rounded-md bg-white">
+@if (isset($randomProducts) && count($randomProducts) > 0)
+    <div class="w-full bg-slate-50 dark:bg-slate-800 text-slate800 dark:text-slate-200 py-2">
+        <div class="container mx-auto">
+            <h1 class="text-center text-lg flex justify-between items-end">
+                <span>Some Random Products</span>
+                <a href="#" class="text-sm dark:text-indigo-300 text-indigo-700">See All</a>
+            </h1>
+            <div
+                class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-2">
+                @foreach ($randomProducts as $product)
+                    <a href="/products/{{ $product->id }}"
+                        class="mx-auto my-2 shadow w-44 sm:w-52 md:w-[185px] lg:w-[200px] xl:w-52 2xl:w-[185px] dark:bg-slate-900/60 dark:text-slate-200 text-slate-700 overflow-hidden rounded-md bg-white">
 
-                    @php
-                        $image = json_decode($product->images, true)[0];
-                        if (strpos($image, 'https://') !== 0 && strpos($image, 'http://') !== 0) {
-                            $imageUrl = Storage::url($image);
-                        } else {
-                            $imageUrl = $image;
-                        }
-                        
-                        $addressGroup = $product->seller->user->municipality->district->district_name . ' (' . $product->seller->user->municipality->municipality_name . ')';
-                        
-                    @endphp
-                    <img src="{{ $imageUrl }}" alt="" class="h-44 sm:h-52 md:h-[185px] lg:h-[200px] xl:h-52 2xl:h-[185px] w-full object-cover rounded-b">
-                    <div class="px-2 py-1">
-                        <h5 class="text-md line-clamp-1" title="{{ $product->product_name }}">
-                            {{ $product->product_name }}</h5>
-                        <p class="text-sm font-bold">
-                            <small class="text-muted" title="{{ $product->price }}">Rs {{ $product->price }}</small>
-                        </p>
-                        <p class="text-sm dark:text-slate-400 text-slate-700 line-clamp-1"
-                            title="{{ $product->seller->shop_name }}">
-                            {{ $product->seller->shop_name }}
-                        </p>
-                        <p class="text-xs line-clamp-1" title="{{ $addressGroup }}">
-                            <span class="font-bold dark:text-slate-300 text-slate-700">{{ $addressGroup }}</span>
-                        </p>
-                    </div>
-                </a>
-            @endforeach
+                        @php
+                            $image = json_decode($product->images, true)[0];
+                            if (strpos($image, 'https://') !== 0 && strpos($image, 'http://') !== 0) {
+                                $imageUrl = Storage::url($image);
+                            } else {
+                                $imageUrl = $image;
+                            }
+                            
+                            $addressGroup = $product->seller->user->municipality->district->district_name . ' (' . $product->seller->user->municipality->municipality_name . ')';
+                            
+                        @endphp
+                        <img src="{{ $imageUrl }}" alt=""
+                            class="h-44 sm:h-52 md:h-[185px] lg:h-[200px] xl:h-52 2xl:h-[185px] w-full object-cover rounded-b">
+                        <div class="px-2 py-1">
+                            <h5 class="text-md line-clamp-1" title="{{ $product->product_name }}">
+                                {{ $product->product_name }}</h5>
+                            <p class="text-sm font-bold">
+                                <small class="text-muted" title="{{ $product->price }}">Rs
+                                    {{ $product->price }}</small>
+                            </p>
+                            <p data-href="{{ route('shop', $product->seller->id) }}"
+                                class="dynamic-link text-sm dark:text-slate-400 text-slate-700 line-clamp-1"
+                                title="{{ $product->seller->shop_name }}">
+                                {{ $product->seller->shop_name }}
+                            </p>
+                            <p class="text-xs line-clamp-1" title="{{ $addressGroup }}">
+                                <span class="font-bold dark:text-slate-300 text-slate-700">{{ $addressGroup }}</span>
+                            </p>
+                        </div>
+                    </a>
+                @endforeach
+            </div>
         </div>
     </div>
-</div>
 @endif
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,38 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.bunny.net">
-  <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-  <!-- Add the slick-theme.css if you want default styling -->
-  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css" />
+    <!-- Add the slick-theme.css if you want default styling -->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css" />
 
-  <!-- Add the slick-theme.css if you want default styling -->
-  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
+    <!-- Add the slick-theme.css if you want default styling -->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick-theme.css" />
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
-  <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 
-  <!-- Scripts -->
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="font-sans antialiased" x-data="{ darkMode: false }" x-init="if (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) { localStorage.setItem('darkMode', JSON.stringify(true)); } darkMode = JSON.parse(localStorage.getItem('darkMode')); $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))" x-cloak>
-  <div x-bind:class="{ 'dark': darkMode === true }" class="h-screen">
-    <div class="bg-slate-200 dark:bg-slate-700 dark:text-slate-300 overflow-auto h-full flex flex-col">
-      @include('customer.toast')
-      @include('customer.navbar')
-      @yield('children')
-    </div>
-  </div>
 
-  <script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
+<body class="font-sans antialiased" x-data="{ darkMode: false }" x-init="if (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) { localStorage.setItem('darkMode', JSON.stringify(true)); } darkMode = JSON.parse(localStorage.getItem('darkMode'));
+$watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))" x-cloak>
+    <div x-bind:class="{ 'dark': darkMode === true }" class="h-screen">
+        <div class="bg-slate-200 dark:bg-slate-700 dark:text-slate-300 overflow-auto h-full flex flex-col">
+            @include('customer.toast')
+            @include('customer.navbar')
+            @yield('children')
+        </div>
+    </div>
+
+    <script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.dynamic-link').click(function(e) {
+                e.preventDefault();
+                // Construct the redirect URL
+                var redirectUrl = $(this).data('href');
+
+                // Redirect to the constructed URL
+                window.location.href = redirectUrl;
+            });
+        });
+    </script>
 </body>
+
 </html>
