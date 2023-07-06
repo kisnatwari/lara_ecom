09b860a061f97d3bb9326fa1c0147d025a9c9703
Krishna Tiwari <kisnatwari@gmail.com>
2023-07-06 18:41:31 +0545

    UsersTableSeeder Updated

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Storage;
 
 class UsersTableSeeder extends Seeder
 {
@@ -15,15 +16,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-         // Clear existing records from the table
-         //DB::table('users')->truncate();
+        // Clear existing records from the table
+        //DB::table('users')->truncate();
 
         // Create user records
         $users = [];
         $faker = Faker::create();
-        $password =Hash::make('password');
+        $password = Hash::make('password');
+        $pictures = Storage::files('public/profile_pics');
 
         for ($i = 1; $i <= 5000; $i++) {
+            $profile_photo = $pictures[array_rand($pictures)];
             $users[] = [
                 'name' => $faker->name,
                 'email' => 'user'.$i.'@example.com',
@@ -31,6 +34,7 @@ class UsersTableSeeder extends Seeder
                 'phone' => ($i - 1) % 758 + 1,
                 'municipality_id' => rand(1, 753), // Replace with the desired range of municipality IDs
                 'ward' => $faker->word(). " ward",
+                'profile_photo' => $profile_photo
             ];
         }
 
